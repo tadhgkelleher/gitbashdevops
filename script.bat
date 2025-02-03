b5f1f12f8b715c9926eddc9f00abf29216b6aa35
tadhgkelleher <tadhgkelleher@gmail.com>
2025-02-03 16:07:45 +0000

    Update script.bat to display 'Job completed' at the end

--- script.bat
+++ script.bat
@@ -9,3 +9,15 @@ type nul > fol_1\1_3.txt
 type nul > fol_2\2_1.txt
 type nul > fol_2\2_2.txt
 type nul > fol_2\2_3.txt
+
+:: Set permissions for _1.txt and _3.txt files (read and write by user only)
+icacls "fol_1\1_1.txt" /grant "%username%:RW"
+icacls "fol_1\1_3.txt" /grant "%username%:RW"
+icacls "fol_2\2_1.txt" /grant "%username%:RW"
+icacls "fol_2\2_3.txt" /grant "%username%:RW"
+
+:: Set permissions for _2.txt files (read, write, and execute by everyone)
+icacls "fol_1\1_2.txt" /grant "Everyone:(R,W,X)"
+icacls "fol_2\2_2.txt" /grant "Everyone:(R,W,X)"
+
+echo Job completed
